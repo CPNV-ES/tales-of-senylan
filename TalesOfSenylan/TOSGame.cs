582d92af8da7c0ea4d187cd9b8fc1bf843136856
JEGARAJASINGAM Senistan <Senistan.JEGARAJASINGAM@cpnv.ch>
2020-09-03 13:28:25 +0200

    Rename Game1 to TOSGame

--- TalesOfSenylan/TOSGame.cs
+++ TalesOfSenylan/TOSGame.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TalesOfSenylan
 {
-    public class Game1 : Game
+    public class TOSGame : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public TOSGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
