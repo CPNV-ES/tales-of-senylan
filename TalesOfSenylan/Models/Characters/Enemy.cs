f2073a1a3651e8f687f17e70bb2313330862c918
dlmw <12473240+dlmw@users.noreply.github.com>
2020-10-01 11:56:27 +0200

    Solve merge conflicts

--- TalesOfSenylan/Models/Characters/Enemy.cs
+++ TalesOfSenylan/Models/Characters/Enemy.cs
@@ -1,15 +1,103 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TalesOfSenylan.Models.Characters
 {
     public class Enemy : Character
     {
+        private float MovementDuration;
+        private TimeSpan CurrentMovementDuration;
+
+        private bool HasMovedLeft = false;
+        private bool HasMovedRight = false;
+        private bool HasMovedTop = false;
+        private bool HasMovedBottom = false;
+
+        private TimeSpan LastDirectionChangeTime;
+
         public Enemy(Dungeon dungeon, Vector2 position) : base(dungeon, position)
         {
+            MovementDuration = Utilities.Utilities.getRandomNumber(1, 3);
+            CurrentMovementDuration = new TimeSpan();
+            LoadContent();
+        }
+
+        public void LoadContent()
+        {
+            Sprite = Dungeon.Content.Load<Texture2D>("orc");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                Sprite,
+                Position,
+                null,
+                Color.White,
+                0f,
+                new Vector2(Sprite.Width / 2, Sprite.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            CurrentMovementDuration = CurrentMovementDuration.Add(gameTime.ElapsedGameTime);
             
+            if (!HasMovedRight)
+            {
+                Position.X += Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (ShouldChangeDirection(gameTime))
+                {
+                    HasMovedRight = true;
+                    LastDirectionChangeTime = gameTime.TotalGameTime;
+                }
+            } else if (!HasMovedBottom)
+            {
+                Position.Y += Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (ShouldChangeDirection(gameTime))
+                {
+                    HasMovedBottom = true;
+                    LastDirectionChangeTime = gameTime.TotalGameTime;
+                }
+            } else if (!HasMovedLeft)
+            {
+                Position.X -= Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (ShouldChangeDirection(gameTime))
+                {
+                    HasMovedLeft = true;
+                    LastDirectionChangeTime = gameTime.TotalGameTime;
+                }
+            } else if (!HasMovedTop)
+            {
+                Position.Y -= Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (ShouldChangeDirection(gameTime))
+                {
+                    HasMovedTop = true;
+                    LastDirectionChangeTime = gameTime.TotalGameTime;
+                }
+
+                if (HasCompletedlap())
+                {
+                    HasMovedRight = false;
+                    HasMovedBottom = false;
+                    HasMovedLeft = false;
+                    HasMovedTop = false;
+                }
+            }
+        }
+
+        private bool ShouldChangeDirection(GameTime gameTime)
+        {
+            return gameTime.TotalGameTime.TotalSeconds - LastDirectionChangeTime.TotalSeconds >= MovementDuration;
+        }
+
+        private bool HasCompletedlap()
+        {
+            return HasMovedRight && HasMovedBottom && HasMovedLeft && HasMovedTop;
         }
     }
 }
--- TalesOfSenylan/Models/Dungeon.cs
+++ TalesOfSenylan/Models/Dungeon.cs
@@ -1,17 +1,17 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using TalesOfSenylan.Models.Characters;
+using TalesOfSenylan.Models.Utilities;
 
 namespace TalesOfSenylan
 {
     public class Dungeon
     {
         public Player Player { get; set; }
-        public Enemy Enemy { get; set; }
+        public List<Enemy> Enemies { get; set; }
         private Vector2 StartPosition;
 
         private int DungeonNumber;
@@ -22,27 +22,47 @@ namespace TalesOfSenylan
         {
             Content = new ContentManager(serviceProvider, "Content");
             DungeonNumber = dungeonNumber;
-            StartPosition = new Vector2(250, 250);
+            StartPosition = new Vector2(200, 200);
             InitializeLevel();
         }
         
         private void InitializeLevel()
         {
             Player = new Player(this, StartPosition);
+            Enemies = new List<Enemy>();
+            // Todo: right now, the number of enemies per level is (DungeonNumber * 5) => make it more "intelligent"
+            // I tried to do it in an Enum before (by hard-coding number of enemies for each level), but didn't find much success.
+            for (int i = 0; i < this.DungeonNumber * 5; i++)
+            {
+                Enemies.Add(new Enemy(this, GenerateRandomStartingPosition()));
+            }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             Player.Draw(gameTime, spriteBatch);
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.Draw(gameTime, spriteBatch);
+            }
         }
 
         public void Update(GameTime gameTime)
         {
             Player.Update(gameTime);
-            if (Player.Collide(Enemy))
+            foreach (Enemy enemy in Enemies)
             {
-                Debug.WriteLine("Collided");
+                enemy.Update(gameTime);
             }
+        }
+
+        public static Vector2 GenerateRandomStartingPosition()
+        {
+            // Todo: position shouldn't be between 20 and 200 => make it respect dungeon bounds.
+            int x = Utilities.getRandomNumber(20, 200);
+            int y = Utilities.getRandomNumber(20, 200);
+
+            return new Vector2(x, y);
         }
     }
 }
