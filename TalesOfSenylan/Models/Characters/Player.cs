ad0e1c64238d466f8b388e36a882d1cb608e410c
dlmw <12473240+dlmw@users.noreply.github.com>
2020-12-15 11:07:58 +0100

    Add Inventory attribute to Player and add a few items

--- TalesOfSenylan/Models/Characters/Player.cs
+++ TalesOfSenylan/Models/Characters/Player.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using TalesOfSenylan.Models.Characters;
 using TalesOfSenylan.Models.Dungeon;
+using TalesOfSenylan.Models.Items;
+using TalesOfSenylan.Models.Items.Weapons;
 
 namespace TalesOfSenylan
 {
@@ -19,6 +21,8 @@ namespace TalesOfSenylan
         private readonly TimeSpan attackRate = new TimeSpan(0, 0, 1); //cooldown attack set to 1sec
         private int c = 1; //Debug counter of the attack
         private TimeSpan nextAttack;
+        private Inventory inventory = new Inventory();
+        
 
         public Player(Dungeon dungeon, Vector2 initialPosition) : base(dungeon, initialPosition)
         {
@@ -28,6 +32,12 @@ namespace TalesOfSenylan
             maxMana = mana = 200;
             hitbox = new RectangleF(position.X - sprite.Width / 2, position.Y - sprite.Height / 2, sprite.Width,
                 sprite.Height);
+
+            Sword s = new Sword("Holy Sword of Fire");
+            Potion p = new Potion("Health Potion (50)");
+            inventory.AddItem(s);
+            inventory.AddItem(p);
+            inventory.AddItem(p);
         }
 
         private State state { get; set; }
